a7f63da9b5633777e3097a056f67c90681f4a1ae
Skylar Tamke <skylar4494@gmail.com>
2017-02-07 17:17:54 -0700

    Add files via upload

--- lab_1/bit_ripple.vhd
+++ lab_1/bit_ripple.vhd
@@ -4,8 +4,8 @@ use ieee.std_logic_1164.all;
 entity bit_ripple is
 	port(	
 	A,B	:in std_logic_vector(7 downto 0);
-	sub	:in std_logic_vector (1 downto 0);
-	S	:out std_logic_vector(7 downto 0);
+	sub	:in std_logic;
+	S		:out std_logic_vector(7 downto 0);
 	Cout 	:out std_logic
 	);
 
@@ -24,17 +24,17 @@ signal C1,C2,C3,C4,C5,C6,C7,C8	:std_logic;  --Carry bits for each of the adders
 signal X1,X2,X3,X4,X5,X6,X7,X8 :std_logic;
 
 begin 
-	X1 <= B(0) XOR sub(0);
-	X2 <= B(1) XOR sub(0);
-	X3 <= B(2) XOR sub(0);
-	X4 <= B(3) XOR sub(0);
-	X5 <= B(4) XOR sub(0);
-	X6 <= B(5) XOR sub(0);
-	X7 <= B(6) XOR sub(0);
-	X8 <= B(7) XOR sub(0);
+	X1 <= B(0) XOR sub;
+	X2 <= B(1) XOR sub;
+	X3 <= B(2) XOR sub;
+	X4 <= B(3) XOR sub;
+	X5 <= B(4) XOR sub;
+	X6 <= B(5) XOR sub;
+	X7 <= B(6) XOR sub;
+	X8 <= B(7) XOR sub;
 	
 	
-	A0: full_adder port map(A(0),X1,sub(0),C1,S(0));	--1
+	A0: full_adder port map(A(0),X1,sub,C1,S(0));	--1
 	A1: full_adder port map(A(1),X2,C1,C2,S(1));	--2
 	A2: full_adder port map(A(2),X3,C2,C3,S(2));	--3
 	A3: full_adder port map(A(3),X4,C3,C4,S(3));	--4
--- lab_1/wrapper.vhd
+++ lab_1/wrapper.vhd
@@ -4,13 +4,13 @@ use ieee.std_logic_1164.all;
 
 
 entity wrapper is 
-    port(
-        Clock          :in std_logic;
-        X,Y            :in std_logic_vector(7 downto 0);    
-        Subtract       :in std_logic_vector(1 downto 0);
-        Sum            :out std_logic_vector(7 downto 0);
-        Carryout       :out std_logic
-        );
+	port(
+		Clock			:in std_logic;
+		X,Y			:in std_logic_vector(7 downto 0);	
+		Subtract		:in std_logic;
+		Sum			:out std_logic_vector(7 downto 0);
+		Carryout		:out std_logic
+		);
 
 end entity;
 
@@ -18,32 +18,31 @@ end entity;
 
 architecture wrapper_arch of wrapper is
 
-component eightbit_cla is
-    port(A,B        :in  std_logic_vector(7 downto 0);
-         Cin        :in  std_logic;
-         Sub        :in  std_logic;
-         Sum        :out std_logic_vector(7 downto 0);
-         Cout       :out std_logic
-         );
-
+component bit_ripple
+	port(	
+	A,B	:in std_logic_vector(7 downto 0);
+	sub	:in std_logic;
+	S		:out std_logic_vector(7 downto 0);
+	Cout 	:out std_logic
+	);
 end component;
 
-    signal A,B    :std_logic_vector (7 downto 0);
-    signal S      :std_logic_vector (1 downto 0);
-
+	signal A,B	:std_logic_vector(7 downto 0);
+	signal S		:std_logic;
+	
 begin 
 
-    process(Clock)
-        begin
+	process(Clock)
+		begin
 
-        if(rising_edge(Clock)) then
-            A <= X;
-            B <= y;
-            S <= Subtract;
+		if(rising_edge(Clock)) then
+			A <= X;
+			B <= y;
+			S <= Subtract;
 
-        end if;
-    end process;
+		end if;
+	end process;
 
-    A1 : eightbit_cla port map(A,B,'0',S(0),Sum,Carryout);
+	A1 : bit_ripple port map(A,B,S,Sum,Carryout);
 
 end architecture;
